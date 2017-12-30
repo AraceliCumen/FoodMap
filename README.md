e6d768880323d9f190bd16929685672721442b0f
AraceliCumen <aycuevam@gmail.com>
2017-12-30 01:17:31 -0500

    Subiendo estructura de carpetas, data, archivos html,js css y assets del proyecto

---
+++ README.md
@@ -0,0 +1,43 @@
+# Proyecto: FoodMap (CaxaFood)
+
+## Objetivos
+
+Crea una web-app que a través de un input pueda filtrar los restaurantes que se encuentran cerca de ti (Tú decides la estructura que tendrán tus datos, puedes crear una lista de restaurantes en un arreglo, en un objeto, de manera individual. El diseño es totalmente libre).
+
+## Flujo de la Aplicación
+
+- Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista principal.
+
+***
+![Vista Splash](assets/img/splash.jpg "Vista Splash")
+***
+
+- En la vista principal se muestran todos los restaurantes "cerca de ti" junto con el input para filtrar tu elección (Los criterios de filtrado son decisión tuya, puede ser por tipo de comida, costos, orden alfabético, etc.).
+
+***
+![Vista principal](assets/img/2.jpg "Vista principal")
+***
+
+- Ya que se hizo la elección del filtro deben mostrarse únicamente aquellos restaurantes que cumplan con la condición.
+
+***
+![Vista principal](assets/img/3.jpg "Vista principal")
+***
+
+- Agrega un evento mouseover a las imágenes, lo que pase con el efecto es decisión tuya (Cambio de tamaño, muestra de texto, cambio de imagen, información, etc).
+
+***
+![Vista principal](assets/img/3.jpg "Vista principal")
+***
+
+- Al seleccionar alguno de los restaurantes, deberá mostrarse la información de este a través de un modal.
+
+***
+![Vista principal](assets/img/5.jpg "Vista principal")
+***
+
+- Una vez cerrado el modal debe volver a la vista principal.
+
+***
+![Vista principal](assets/img/6.jpg "Vista principal")
+***
---
+++ js/data.js
@@ -0,0 +1,56 @@
+var data = {
+  'MUELLE 33': {
+    'Ubicacion': {
+      'Jiron': 'Rafael Olascoga N° 133',
+      'Urbanización': 'Horacio Zeballos',
+      'Ciudad': 'Cajamarca'
+    },
+    'Telefono': '076-632925',
+    'Messenger': '@Muelle33restaurantcevicheria',
+    'Horario': '10:00 - 17:00',
+    'email': 'muelle33peru@hotmail.com',
+    'Platos': ['Combinado 33', 'Arroz con Mariscos', 'Ceviche 3 Sabores', 'Ceviche Especial', 'Ceviche Mixto', 'Ceviche Muelle 33', 'Chicharron de Mariscos', 'Combo Universitario', 'Sudado de Pescado', 'Trio'],
+    'Página-Oficial': 'https://www.facebook.com/Muelle33restaurantcevicheria/',
+    'Calificacion': 5
+  }, 
+  'EL TIESTO RESTOBAR CAMPESTRE': {
+    'Ubicacion': {
+      'Avenida': 'Hoyos Rubio - Grifo: "Las Torrecitas"',
+      'Urbanización': 'Toribio Casanova',
+      'Ciudad': 'Cajamarca'
+    },
+    'Telefono': '076-365417',
+    'Messenger': '@elTiestoRestaurantCampestre',
+    'Horario': '9:00 - 17:00',
+    'email': 'restaurant.eltiesto@hotmail.com',
+    'Platos': ['Cecina Shilpida', 'Lomito Saltado', 'Pollo al Barro'],
+    'Página-Oficial': 'https://www.facebook.com/ElTiestoRestaurantCampestre/',
+    'Calificacion': 4
+  }, 
+  'EL ASADOR': {
+    'Ubicacion': {
+      'Jiron': '2 de Mayo Esquina con Manuel Seoane',
+      'Ciudad': 'Cajamarca'
+    },
+    'Telefono': '956217173',
+    'Messenger': '@elAsadorGrill',
+    'Horario': '19:00 - 23:00',
+    'email': 'elasador@hotmail.com',
+    'Platos': ['Arroz Chaufa con chancho al cilindro', 'Chanchito al Cilindro', 'Fetuchinis a la Huancaina con chancho al cilindro' , 'Fetuchinis al Pesto con chancho al cilindro', 'Pollo al Cilindro'],
+    'Página-Oficial': 'https://www.facebook.com/ElAsadorGrill/',
+    'Calificacion': 5
+  }, 
+  'GRAN RESTAURANT EL ZARCO': {
+    'Ubicacion': {
+      'Jiron': 'Del Batán N° 252',
+      'Ciudad': 'Cajamarca'
+    },
+    'Telefono': '076-369022',
+    'Messenger': '@rest.elzarco',
+    'Horario': '7:00 - 23:00',
+    'email': 'elzarco_2005@hotmail.com',
+    'Platos': ['Arroz con pato', 'Cecina Shilpida', 'Chicharron de pescado', 'Chupe verde', 'Cuy frito', 'Lechon a la caja china', 'Panque con manjar blanco', 'Picante de Cuy', 'Tallarin con pollo', 'Tallarines en salsa verde', 'Triple'],
+    'Página-Oficial': 'https://www.facebook.com/rest.elzarco/',
+    'Calificacion': 3.5
+  }
+}
