065cd03ddf874ca09d22a90c83d74634d30e38f3
AraceliCumen <aycuevam@gmail.com>
2018-01-02 18:04:19 -0500

    Actualizando data

--- js/data.js
+++ js/data.js
@@ -1,168 +1,200 @@
 var data = {
   'MUELLE 33': {
-    'Ubicacion': {
-      'Jiron': 'Rafael Olascoga N° 133',
-      'Urbanización': 'Horacio Zeballos',
-      'Ciudad': 'Cajamarca'
+    img: 'assets/img/principal3.png',
+    Ubicacion: {
+      Jiron: 'Rafael Olascoga N° 133',
+      Urbanización: 'Horacio Zeballos',
+      Ciudad: 'Cajamarca'
     },
-    'Telefono': '076-632925',
-    'Messenger': '@Muelle33restaurantcevicheria',
-    'Horario': '10:00 - 17:00',
-    'email': 'muelle33peru@hotmail.com',
-    'Platos': [{ 
-      'Plato': 'Combinado 33', 
-      'Precio': '35 Soles'
-    }, 
-    { 
-      'Plato': 'Arroz con Mariscos', 
-      'Precio': '22 Soles'
-    },
-    { 
-      'Plato': 'Ceviche 3 Sabores', 
-      'Precio': '22 Soles'
-    },
-    { 
-      'Plato': 'Ceviche Especial', 
-      'Precio': '28 Soles'
-    },
-    { 
-      'Plato': 'Ceviche Muelle 33', 
-      'Precio': '27 Soles'
-    },
-    { 
-      'Plato': 'Chicharron de Mariscos', 
-      'Precio': '22 Soles'
-    },
-    { 
-      'Plato': 'Combo Universitario', 
-      'Precio': '22 Soles'
-    },
-    { 
-      'Plato': 'Sudado de Pescado', 
-      'Precio': '20 Soles'
-    },
-    { 
-      'Plato': 'Trio', 
-      'Precio': '22 Soles'
+    Telefono: '076-632925',
+    Messenger: '@Muelle33restaurantcevicheria',
+    Horario: '10:00 - 17:00',
+    email: 'muelle33peru@hotmail.com',
+    Platos: [{
+      Plato1: 'Combinado 33',
+      Precio: '35 Soles'
     },
     {
-      'Plato': 'Ceviche Mixto', 
-      'Precio': '23 Soles'
+      Plato2: 'Arroz con Mariscos',
+      img: 'assets/img/muelle33/arrozconmariscos.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato3: 'Ceviche 3 Sabores',
+      img: 'assets/img/muelle33/ceviche3sabores33.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato4: 'Ceviche Especial',
+      img: 'assets/img/muelle33/cevicheespecial.jpg',
+      Precio: '28 Soles'
+    },
+    {
+      Plato5: 'Ceviche Muelle 33',
+      img: 'assets/img/muelle33/cevichemuelle33.jpg',
+      Precio: '27 Soles'
+    },
+    {
+      Plato6: 'Chicharron de Mariscos',
+      img: 'assets/img/muelle33/chicharrondemariscos.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato7: 'Combo Universitario',
+      img: 'assets/img/muelle33/combouniversitario33.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato8: 'Sudado de Pescado',
+      img: 'assets/img/muelle33/sudadodepescado33.jpg',
+      Precio: '20 Soles'
+    },
+    {
+      Plato9: 'Trio',
+      img: 'assets/img/muelle33/trio33.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato10: 'Ceviche Mixto',
+      img: 'assets/img/muelle33/cevichemixto.jpg',
+      Precio: '23 Soles'
     }],
     'Página-Oficial': 'https://www.facebook.com/Muelle33restaurantcevicheria/',
-    'Calificacion': 5
-  }, 
+    Calificacion: 5
+  },
   'EL TIESTO RESTOBAR CAMPESTRE': {
-    'Ubicacion': {
-      'Avenida': 'Hoyos Rubio - Grifo: "Las Torrecitas"',
-      'Urbanización': 'Toribio Casanova',
-      'Ciudad': 'Cajamarca'
+    img: 'assets/img/principal2.png',
+    Ubicacion: {
+      Avenida: 'Hoyos Rubio - Grifo: "Las Torrecitas"',
+      Urbanización: 'Toribio Casanova',
+      Ciudad: 'Cajamarca'
     },
-    'Telefono': '076-365417',
-    'Messenger': '@elTiestoRestaurantCampestre',
-    'Horario': '9:00 - 17:00',
-    'email': 'restaurant.eltiesto@hotmail.com',
-    'Platos': [{ 
-      'Plato': 'Cecina Shilpida', 
-      'Precio': '20 Soles'
-    }, 
-    { 
-      'Plato': 'Lomito Saltado', 
-      'Precio': '21 Soles'
+    Telefono: '076-365417',
+    Messenger: '@elTiestoRestaurantCampestre',
+    Horario: '9:00 - 17:00',
+    email: 'restaurant.eltiesto@hotmail.com',
+    Platos: [{
+      Plato1: 'Cecina Shilpida',
+      img: 'assets/img/tiesto/cecinashilpida.jpg',
+      Precio: '20 Soles'
     },
     {
-      'Plato': 'Pollo al Barro', 
-      'Precio': '25 Soles'
+      Plato2: 'Lomito Saltado',
+      img: 'assets/img/tiesto/lomitosaltado.jpg',
+      Precio: '21 Soles'
+    },
+    {
+      Plato3: 'Pollo al Barro',
+      img: 'assets/img/tiesto/polloalbarro.jpg',
+      Precio: '25 Soles'
     }],
     'Página-Oficial': 'https://www.facebook.com/ElTiestoRestaurantCampestre/',
     'Calificacion': 4
-  }, 
+  },
   'EL ASADOR': {
-    'Ubicacion': { 
-      'Jiron': '2 de Mayo Esquina con Manuel Seoane',
-      'Ciudad': 'Cajamarca'
+    img: 'assets/img/principal1.png',
+    Ubicacion: {
+      Jiron: '2 de Mayo Esquina con Manuel Seoane',
+      Ciudad: 'Cajamarca'
     },
-    'Telefono': '956217173',
-    'Messenger': '@elAsadorGrill',
-    'Horario': '19:00 - 23:00',
-    'email': 'elasador@hotmail.com',
-    'Platos': [{ 
-      'Plato': 'Arroz Chaufa con chancho al cilindro', 
-      'Precio': '20 Soles'
-    }, 
-    { 
-      'Plato': 'Chanchito al Cilindro', 
-      'Precio': '22 Soles'
-    },
-    { 
-      'Plato': 'Pollo al Cilindro', 
-      'Precio': '18 Soles'
-    },
-    { 
-      'Plato': 'Fetuchinis al Pesto con chancho al cilindro', 
-      'Precio': '22 Soles'
+    Telefono: '956217173',
+    Messenger: '@elAsadorGrill',
+    Horario: '19:00 - 23:00',
+    email: 'elasador@hotmail.com',
+    Platos: [{
+      Plato: 'Arroz Chaufa con chancho al cilindro',
+      img: 'assets/img/elasador/arrozchaufoconchancho.png',
+      Precio: '20 Soles'
     },
     {
-      'Plato': 'Fetuchinis a la Huancaina con chancho al cilindro', 
-      'Precio': '23 Soles'
+      Plato: 'Chanchito al Cilindro',
+      img: 'assets/img/elasador/chanchitoalcilindro.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato: 'Pollo al Cilindro',
+      img: 'assets/img/elasador/polloalcilindro.jpg',
+      Precio: '18 Soles'
+    },
+    {
+      Plato: 'Fetuchinis al Pesto con chancho al cilindro',
+      img: 'assets/img/elasador/fetuchinisalpesto.jpg',
+      Precio: '22 Soles'
+    },
+    {
+      Plato: 'Fetuchinis a la Huancaina con chancho al cilindro',
+      img: 'assets/img/elasador/fetuchinisalahuncaina.png',
+      Precio: '23 Soles'
     }],
     'Página-Oficial': 'https://www.facebook.com/ElAsadorGrill/',
     'Calificacion': 5
-  }, 
+  },
   'GRAN RESTAURANT EL ZARCO': {
-    'Ubicacion': {
-      'Jiron': 'Del Batán N° 252',
-      'Ciudad': 'Cajamarca'
+    img: 'assets/img/principal4.png',
+    Ubicacion: {
+      Jiron: 'Del Batán N° 252',
+      Ciudad: 'Cajamarca'
     },
-    'Telefono': '076-369022',
-    'Messenger': '@rest.elzarco',
-    'Horario': '7:00 - 23:00',
-    'email': 'elzarco_2005@hotmail.com',
-    'Platos': [{ 
-      'Plato': 'Arroz con pato', 
-      'Precio': '22 Soles'
-    }, 
-    { 
-      'Plato': 'Cecina Shilpida', 
-      'Precio': '20 Soles'
-    },
-    { 
-      'Plato': 'Chicharron de pescado', 
-      'Precio': '18 Soles'
-    },
-    { 
-      'Plato': 'Chupe verde', 
-      'Precio': '12 Soles'
-    },
-    { 
-      'Plato': 'Lechon a la caja china', 
-      'Precio': '30 Soles'
-    },
-    { 
-      'Plato': 'Panque con manjar blanco', 
-      'Precio': '10 Soles'
-    },
-    { 
-      'Plato': 'Picante de Cuy', 
-      'Precio': '25 Soles'
-    },
-    { 
-      'Plato': 'Tallarin con pollo', 
-      'Precio': '18 Soles'
-    },
-    { 
-      'Plato': 'Tallarines en salsa verde', 
-      'Precio': '18 Soles'
-    },
-    { 
-      'Plato': 'Triple', 
-      'Precio': '15 Soles'
+    Telefono: '076-369022',
+    Messenger: '@rest.elzarco',
+    Horario: '7:00 - 23:00',
+    email: 'elzarco_2005@hotmail.com',
+    Platos: [{
+      Plato: 'Arroz con pato',
+      img: 'assets/img/elzarco/arrozconpato.jpg',
+      Precio: '22 Soles'
     },
     {
-      'Plato': 'Cuy frito', 
-      'Precio': '23 Soles'
+      Plato: 'Cecina Shilpida',
+      img: 'assets/img/elzarco/cecinashilpida.jpg',
+      Precio: '20 Soles'
+    },
+    {
+      Plato: 'Chicharron de pescado',
+      img: 'assets/img/elzarco/chicharróndepescadoconpapasyensalada.jpg',
+      Precio: '18 Soles'
+    },
+    {
+      Plato: 'Chupe verde',
+      img: 'assets/img/elzarco/chupeverde.jpg',
+      Precio: '12 Soles'
+    },
+    {
+      Plato: 'Lechon a la caja china',
+      img: 'assets/img/elzarco/lechonalacajachina.jpg',
+      Precio: '30 Soles'
+    },
+    {
+      Plato: 'Panque con manjar blanco',
+      img: 'assets/img/elzarco/panquequeconmanjarblanco.jpg',
+      Precio: '10 Soles'
+    },
+    {
+      Plato: 'Picante de Cuy',
+      img: 'assets/img/elzarco/picantedecuy.jpg',
+      Precio: '25 Soles'
+    },
+    {
+      Plato: 'Tallarin con pollo',
+      img: 'assets/img/elzarco/tallarinconpollo.jpg',
+      Precio: '18 Soles'
+    },
+    {
+      Plato: 'Tallarines en salsa verde',
+      img: 'assets/img/elzarco/tallarinensalsaverde.jpg',
+      Precio: '18 Soles'
+    },
+    {
+      Plato: 'Triple',
+      img: 'assets/img/elzarco/triple.jpg',
+      Precio: '15 Soles'
+    },
+    {
+      Plato: 'Cuy frito',
+      img: 'assets/img/elzarco/cuyfritoconarrozypapacriolla.jpg',
+      Precio: '23 Soles'
     }],
     'Página-Oficial': 'https://www.facebook.com/rest.elzarco/',
-    'Calificacion': 3.5
+    Calificacion: 3.5
   }
 }
--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,15 @@
 // Accediendo a la data
 // console.log(data);
-$(document).ready(function () {
+$(document).ready(function() {
   alert('Ingrese uno de los Restaurants: Muelle 33, EL tiesto Restobar  Campestre, El Asador, Gran Restaurant el Zarco');
   var arrNameRest = [];
-  $.each(data, function (i, item) {
+  $.each(data, function(i, item) {
     arrNameRest.push(i);
   });
 
 
-  $('#search').click(function () {
-    $.each(arrNameRest, function (i, element) {
+  $('#search').click(function() {
+    $.each(arrNameRest, function(i, element) {
       // console.log(typeof(arrNameRest[i]));
       // debugger;
       if ($('#list-restaurant').val().toUpperCase() === arrNameRest[i]) {
