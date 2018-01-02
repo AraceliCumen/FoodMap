76654595a9bdf98026dfad2e7210badb77c5effe
AraceliCumen <aycuevam@gmail.com>
2018-01-02 11:27:38 -0500

    Añadiendo vistas

--- js/principal.js
+++ js/principal.js
@@ -1,24 +1,25 @@
 // Accediendo a la data
 // console.log(data);
 $(document).ready(function () {
+  alert('Ingrese uno de los Restaurants: Muelle 33, EL tiesto Restobar  Campestre, El Asador, Gran Restaurant el Zarco');
   var arrNameRest = [];
   $.each(data, function (i, item) {
     arrNameRest.push(i);
   });
 
-  
-  $('#search').click(function() {
-    $.each(arrNameRest, function (i,element) {
-      console.log(arrNameRest[i]);
+
+  $('#search').click(function () {
+    $.each(arrNameRest, function (i, element) {
+      // console.log(typeof(arrNameRest[i]));
       // debugger;
-      // if ($('#list-restaurant').val().toUpperCase() === arrNameRest[i]) {
-        
-      //   localStorage.restaurant = $('#list-restaurant').val();
-      //   window.location.href = 'secundaria.html';
-      // } else {
-      //   $('#list-restaurant').val('');
-      //   alert('Ingrese uno de los Restaurants: Muelle 33, EL tiesto Restobar  Campestre, El Asador, Gran Restaurant el Zarco');
-      // }
-    })
+      if ($('#list-restaurant').val().toUpperCase() === arrNameRest[i]) {
+        localStorage.restaurant = $('#list-restaurant').val();
+        console.log('encontrado');
+        window.location.href = 'secundaria.html';
+      } else {
+        alert('Ingrese uno de los Restaurants: Muelle 33, EL tiesto Restobar  Campestre, El Asador, Gran Restaurant el Zarco');
+      }
+    });
+    $('#list-restaurant').val('');
   });
 });
---
+++ js/secundaria.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $('#title-Restaurant').text(localStorage.restaurant.toUpperCase());
+});
